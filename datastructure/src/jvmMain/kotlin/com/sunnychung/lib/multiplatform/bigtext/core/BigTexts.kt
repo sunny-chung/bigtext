736fa735f54fff9401fe497cefa134110c6a1130
Sunny Chung <sunnychung@live.hk>
2025-01-05 12:58:54 +0800

    add more BigText factory functions

--- datastructure/src/jvmMain/kotlin/com/sunnychung/lib/multiplatform/bigtext/core/BigTexts.kt
+++ datastructure/src/jvmMain/kotlin/com/sunnychung/lib/multiplatform/bigtext/core/BigTexts.kt
@@ -1,7 +1,21 @@
 package com.sunnychung.lib.multiplatform.bigtext.core
 
-fun BigText.Companion.createFromLargeString(initialContent: String) = BigTextImpl().apply {
-    log.d { "createFromLargeString ${initialContent.length}" }
-    append(initialContent)
-    isUndoEnabled = true
-}
+fun BigText.Companion.createFromLargeString(initialContent: String) = createFromString(initialContent)
+
+fun BigText.Companion.createFromSmallString(initialContent: String) =
+    createFromString(initialContent, chunkSize = 2 * 1024, undoHistoryCapacity = 100) // 2 KB
+
+fun BigText.Companion.createFromTinyString(initialContent: String) =
+    createFromString(initialContent, chunkSize = 32, undoHistoryCapacity = 10) // 32 B
+
+fun BigText.Companion.createFromString(
+    initialContent: String,
+    chunkSize: Int = 2 * 1024 * 1024,
+    undoHistoryCapacity: Int = 1000,
+    parallelism: Int = 30,
+) =
+    BigTextImpl(chunkSize = chunkSize, undoHistoryCapacity = undoHistoryCapacity, parallelism = parallelism).apply {
+        log.d { "createFromLargeString ${initialContent.length}" }
+        append(initialContent)
+        isUndoEnabled = true
+    }
--- ui-composable/src/jvmMain/kotlin/com/sunnychung/lib/multiplatform/bigtext/ux/JetpackComposeBigText.kt
+++ ui-composable/src/jvmMain/kotlin/com/sunnychung/lib/multiplatform/bigtext/ux/JetpackComposeBigText.kt
@@ -6,7 +6,24 @@ import com.sunnychung.lib.multiplatform.bigtext.core.BigText
 import com.sunnychung.lib.multiplatform.bigtext.core.BigTextImpl
 import com.sunnychung.lib.multiplatform.bigtext.util.AnnotatedStringBuilder
 
-fun BigText.Companion.createFromLargeAnnotatedString(initialContent: AnnotatedString) = BigTextImpl(
+fun BigText.Companion.createFromLargeAnnotatedString(initialContent: AnnotatedString) =
+    createFromAnnotatedString(initialContent)
+
+fun BigText.Companion.createFromSmallAnnotatedString(initialContent: AnnotatedString) =
+    createFromAnnotatedString(initialContent, chunkSize = 2 * 1024, undoHistoryCapacity = 100) // 2 KB
+
+fun BigText.Companion.createFromTinyAnnotatedString(initialContent: AnnotatedString) =
+    createFromAnnotatedString(initialContent, chunkSize = 32, undoHistoryCapacity = 10) // 32 B
+
+fun BigText.Companion.createFromAnnotatedString(
+    initialContent: AnnotatedString,
+    chunkSize: Int = 2 * 1024 * 1024,
+    undoHistoryCapacity: Int = 1000,
+    parallelism: Int = 30,
+) = BigTextImpl(
+    chunkSize = chunkSize,
+    undoHistoryCapacity = undoHistoryCapacity,
+    parallelism = parallelism,
     textBufferFactory = { AnnotatedStringTextBuffer(it) },
 //    charSequenceBuilderFactory = { AnnotatedString.Builder(it) },
 //    charSequenceFactory = { (it as AnnotatedString.Builder).toAnnotatedString() },
